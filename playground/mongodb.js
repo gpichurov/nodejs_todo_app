1c89270ae20e5edfb8dc7c108c722fb537448313
Georgi <georgi_pichurov@abv.bg>
2019-01-19 19:03:38 +0200

    com

--- playground/mongodb.js
+++ playground/mongodb.js
@@ -20,8 +20,36 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
         //
         // });
 
-        db.collection('Todos').find({_id: new ObjectID('5c3121389f2c9e6f49ba3ef6')}).toArray().then((docs) => {
-            console.log(docs);
+        // db.collection('Todos').find({_id: new ObjectID('5c3121389f2c9e6f49ba3ef6')}).toArray().then((docs) => {
+        //     console.log(docs);
+        // }, (err) => {
+        //     console.log(err);
+        // });
+
+        // db.collection('Todos').deleteMany({_id: new ObjectID('5c3121389f2c9e6f49ba3ef6')}).then((result) => {
+        //     console.log(result);
+        // }, (err) => {
+        //     console.log(err);
+        // });
+
+        // db.collection('Todos').deleteOne({_id: new ObjectID('5c3121389f2c9e6f49ba3ef6')}).then((result) => {
+        //     console.log(result);
+        // }, (err) => {
+        //     console.log(err);
+        // });
+
+        db.collection('Todos').findOneAndDelete({_id: new ObjectID('5c3a48717b304a46cbca7886')}).then((result) => {
+            console.log(result);
+        }, (err) => {
+            console.log(err);
+        });
+
+        db.collection('Todos').findOneAndUpdate({_id: new ObjectID('5c31e4090fe7e92446f71f4f')}, {
+            $set: {text: 'New Text'}
+        }, {
+            returnOriginal: false
+        }).then((result) => {
+            console.log(result);
         }, (err) => {
             console.log(err);
         });
