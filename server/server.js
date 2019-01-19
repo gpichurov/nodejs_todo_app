b4bb5a6fd8f88daf64aeb83d7a94afff66b80a8a
Georgi <georgi_pichurov@abv.bg>
2019-01-20 00:28:27 +0200

    commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-let express = require('express');
-let bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 let mongoose = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -26,6 +27,60 @@ app.post('/todos', (req, res) => {
     });
 
     console.log(todo);
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    Todo.findById(id).then((todo) => {
+        res.send({todo});
+    }, (e) => {
+        res.status(400).send(e);
+
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    Todo.findByIdAndDelete(id).then((todo) => {
+        res.send({todo});
+    }, (e) => {
+        res.status(400).send(e);
+
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed'])
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+
+
+
+    Todo.findByIdAndUpdate(id, {$set: body}).then((todo) => {
+        if (!todo) {
+            res.send.status(404).send();;
+        }
+        res.send({todo});
+    }, (e) => {
+        res.status(400).send(e);
+
+    });
 });
 
 app.listen(3000, () => {
